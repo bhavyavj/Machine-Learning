3f716021eee07c555b03e1cfd3941e5e79e31d8f
bhavyavj <39678373+bhavyavj@users.noreply.github.com>
2019-06-24 04:20:40 +0530

    Delete pynumber2.py

--- pynumber2.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-row_list=[]
-col_list=[[]]
-for i in range(8):
-  for j in range(2):
-    row_list.append(np.random.randint(100,195))
-    row_list.append(row_list[0]+5)
-    col_list.append([row_list]) 
